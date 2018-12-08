93b4ea6cb90d8eeef334afa12927c5407f016522
Zil Vyas <zvyas08@gmail.com>
2018-12-08 14:14:10 -0500

    Add progress spinner for new workouts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from "@angular/core";
 
 import { MatButtonModule,
+         MatCardModule,
          MatCheckboxModule,
          MatIconModule, 
          MatInputModule, 
          MatDatepickerModule,
          MatListModule,
          MatNativeDateModule,
+         MatProgressSpinnerModule,
+         MatSelectModule,
          MatSidenavModule,
+         MatTabsModule,
          MatToolbarModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatListModule, MatNativeDateModule, 
-            MatDatepickerModule, MatIconModule, MatInputModule, MatSidenavModule,
-            MatToolbarModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatListModule, MatNativeDateModule, 
-            MatDatepickerModule, MatIconModule, MatInputModule, MatSidenavModule,
-            MatToolbarModule]
+  imports: [MatButtonModule, MatCardModule, MatCheckboxModule, MatListModule, MatNativeDateModule, 
+            MatDatepickerModule, MatIconModule, MatInputModule, MatSelectModule, MatSidenavModule,
+            MatTabsModule, MatToolbarModule, MatProgressSpinnerModule],
+  exports: [MatButtonModule, MatCardModule, MatCheckboxModule, MatListModule, MatNativeDateModule, 
+            MatDatepickerModule, MatIconModule, MatInputModule, MatSelectModule, MatSidenavModule,
+            MatTabsModule, MatToolbarModule, MatProgressSpinnerModule]
 })
 export class MaterialModule {}
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
 
+  onGoingTraining = false;
+  
   constructor() { }
 
   ngOnInit() {
