c0ac8a6e3fec2d61075d9216318b5d871737760c
Zil Vyas <zvyas08@gmail.com>
2018-12-31 17:20:18 -0500

    configure firebase for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import * as env from './../../environment_variables';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: env.FIREBASE_API_KEY,
+    authDomain: env.FIREBASE_AUTH,
+    databaseURL: env.FIREBASE_DATABASE,
+    projectId: "ng-fitness-tracker-85bc7",
+    storageBucket: env.FIREBASE_STORAGE,
+    messagingSenderId: env.FIREBASE_MESSAGE_SENDER
+  }
 };
